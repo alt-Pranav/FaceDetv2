97a27c33cc9815514bf3c85afc11ec6bcd345ae1
alt-Pranav <74000887+alt-Pranav@users.noreply.github.com>
2023-06-03 14:28:42 +0530

    #1

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import cv2
+from mtcnn import MTCNN
+
+detector = MTCNN()
+
+def detect_bounding_box(frame):
+    # detect faces in the image
+    faces = detector.detect_faces(frame)
+    #for face in faces:
+    #   print(face) 
+    
+    for face in faces:
+        x, y, width, height = face['box']
+        conf = face['confidence']
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 4)
+        cv2.putText(frame, str(conf)[:4], (x+width, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+    return faces
+
+def main():
+
+    st.title("Face Detector")
+
+    checked = st.checkbox(label="Start Video Feed", value=False)
+
+    if checked:
+
+        FRAME_WINDOW = st.image([])
+        videoFeed = cv2.VideoCapture(0)
+
+        while True:
+            _, frame = videoFeed.read()
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            faces = detect_bounding_box(frame=frame)
+
+            FRAME_WINDOW.image(frame)
+
+    else:
+        st.write("Click to start")
+
+
+if __name__ == "__main__":
+    main()
